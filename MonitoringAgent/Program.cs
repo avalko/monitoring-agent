5eb0e8433544d4e19be962118ebaf8bfc0a8fc29
Artem Valko <wolfphone1@gmail.com>
2017-09-23 11:43:00 +0300

    Добавьте файлы проекта.

---
+++ MonitoringAgent/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Loader;
+using System.Threading;
+
+namespace MonitoringAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started monitoring...");
+            AssemblyLoadContext.Default.Unloading += delegate
+            {
+                Console.WriteLine("Good bye!");
+            };
+            
+            while (true)
+            {
+                // Every second
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}

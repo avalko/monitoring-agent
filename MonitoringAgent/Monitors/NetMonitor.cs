78e932caf69b0bcacaacedd3c4ae404601151ca7
Artem Valko <wolfphone1@gmail.com>
2017-09-24 17:22:55 +0300

    Update base monitor

---
+++ MonitoringAgent/Monitors/NetMonitor.cs
@@ -0,0 +1,18 @@
+﻿namespace MonitoringAgent.Monitors
+{
+    [Monitor("title")]
+    class NetMonitor : BaseMonitor
+    {
+        public override void Init()
+        {
+            Json.Your = 0;
+            Json.Custom = "hello";
+            Json.Data = true;
+        }
+
+        public override void Update()
+        {
+            ++Json.Your;
+        }
+    }
+}
